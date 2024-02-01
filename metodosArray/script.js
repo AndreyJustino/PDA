ae9b4c40a199c25105c17b7e8061193e2e8bb835
AndreyJustino <andreyjustino2020@gmail.com>
2024-02-01 17:10:53 -0300

    metodos de array

---
+++ metodosArray/script.js
@@ -0,0 +1,16 @@
+let nomes =  ["Gil", "Kauã", "Leticia"]
+
+nomes.push("Luna")
+console.log(`linha 4  ${nomes}`)
+
+nomes.unshift("simon")
+console.log(`linha 6 ${nomes}`)
+
+nomes.pop()
+console.log(`linha 8  ${nomes}`)
+
+nomes.pop()
+console.log(`linha 10  ${nomes}`)
+
+nomes.push("Luna")
+console.log(`linha 12 ${nomes}`)
