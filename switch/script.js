c6dc560e2ab31543aa0f8fbc122cc0c5e37c8986
AndreyJustino <andreyjustino2020@gmail.com>
2024-03-21 17:22:28 -0300

    switch case

---
+++ switch/script.js
@@ -0,0 +1,22 @@
+function nomeCase(){
+    let input = document.getElementById("input").value
+
+    switch (input) {
+        case "Ruan":
+            console.log("Tenho um filho (a) com esse nome")
+            break;
+    
+        case "Pedro":
+            console.log("Tenho um filho (a) com esse nome")
+            break;
+
+        case "Katarina":
+            console.log("Tenho um filho (a) com esse nome")
+            break;
+
+        default:
+            console.log("Que nome lindo o seu!")   
+            break;
+    }
+    
+}
